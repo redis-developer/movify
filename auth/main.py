337d7cea4aacd1eec80f2e13ec3c048f40d18151
Matthias Hasler <matthias@hasler.fr>
2021-05-02 15:41:04 +0200

    google login

--- auth/main.py
+++ auth/main.py
@@ -1,154 +1,56 @@
-import os
-from datetime import datetime, timedelta
-from typing import Optional
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
 from pydantic import BaseModel
+from fastapi import FastAPI, Request, Depends, HTTPException
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = os.environ['secret_key']
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "matthias": {
-        "username": "matthias",
-        "hashed_password": "$2b$12$uEPS4k5.47timo1OWLinS.URPiZd25SiokvApgRfLPg0YBr2KrN8e",
+config = Config('.env')  # read config from .env file
+oauth = OAuth(config)
+oid = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=oid,
+    client_kwargs={
+        'scope': 'openid email profile'
     }
-}
+)
+
+app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=config('SECRET_KEY'))
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+@app.get('/login')
+async def login(request: Request):
+    # absolute url for callback
+    # we will define it below
+    redirect_uri = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, redirect_uri)
 
 
-class TokenData(BaseModel):
-    username: Optional[str] = None
+@app.get('/auth')
+async def auth(request: Request):
+    token = await oauth.google.authorize_access_token(request)
+    user = await oauth.google.parse_id_token(request, token)
+    request.session['user'] = user
+    # persist to DB
+    return RedirectResponse('me')
 
 
 class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
+    name: str
+    email: str
+    picture: str
 
 
-class UserInDB(User):
-    hashed_password: str
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-app = FastAPI()
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(username: str):
-    db = fake_users_db
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(username)
+def get_user(request: Request) -> User:
+    user = request.session.get('user')
     if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
+        raise HTTPException(status_code=403, detail="Unauthenticated")
+    return User(**user)
+
+
+@app.get('/me')
+async def me(request: Request, user: User = Depends(get_user)):
     return user
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.post("/signup")
-async def signup(username, password):
-    if username in fake_users_db:
-        return 'nope'
-    else:
-        fake_users_db[username] = {
-            'username': username,
-            'hashed_password': get_password_hash(password),
-        }
-    return 'OK'
-
-
-###############################################################################
-# END OF AUTH
-###############################################################################
-
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
--- auth/run.sh
+++ auth/run.sh
@@ -1,4 +1,8 @@
-# openssl rand -hex 32 > secret_key
-export secret_key=$(cat secret_key)
+[ -f .env ] && uvicorn main:app --reload \
+	|| cat <<EOF
+pls create a .env file containing
 
-uvicorn main:app --reload
+GOOGLE_CLIENT_ID=378316070364-3ef899lco4oaltu7s1agcq109igsset4.apps.googleusercontent.com
+GOOGLE_CLIENT_SECRET= go to https://console.cloud.google.com/apis/credentials
+SECRET_KEY= generate with openssl rand -hex 32
+EOF
